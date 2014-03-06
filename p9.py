2b508cc346842f18c739b8392711447dd65cdba6
Tobias <schmid.tobias@bluewin.ch>
2014-03-06 21:08:17 +0100

    solved problem 9

--- p9.py
+++ p9.py
@@ -1,1 +1,10 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
+
+
+for c in range(1000, 0, -1):
+    for b in range(c, 0, -1):
+        a = 1000 - c - b
+
+        if 0 < a < b and a ** 2 + b ** 2 == c ** 2:
+            print a*b*c
+
